00efdeec9db5fcb9fba3c6d0d544ddd64aa7f36e
Abinet <agonaferdegefe@gmail.com>
2026-03-13 05:26:40 +0300

    Initial commit with real theme files

---
+++ assets/js/audio/audio-reader.js
@@ -0,0 +1,35 @@
+const audio = document.getElementById("audioPlayer");
+
+function setSpeed(rate) {
+    if(audio){
+        audio.playbackRate = rate;
+    }
+}
+console.log("working..")
+
+const audio = document.getElementById("audioPlayer");
+
+function setSpeed(rate) {
+    if(audio){
+        audio.playbackRate = rate;
+    }
+}
+console.log("working..")
+
+const audio = document.getElementById("audioPlayer");
+
+function setSpeed(rate) {
+    if(audio){
+        audio.playbackRate = rate;
+    }
+}
+console.log("working..")
+
+const audio = document.getElementById("audioPlayer");
+
+function setSpeed(rate) {
+    if(audio){
+        audio.playbackRate = rate;
+    }
+}
+console.log("working..")
